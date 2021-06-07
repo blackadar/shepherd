d21975f7d37f6144c3e8c3197d2bd26114300e89
Jordan Blackadar <jordan.blackadar@outlook.com>
2021-06-07 01:32:48 -0400

    SPD-2 Node Telemetry Add Battery Metric.

--- node/__main__.py
+++ node/__main__.py
@@ -7,6 +7,7 @@ from node.telemetry.metrics.cpu import CPU
 from node.telemetry.metrics.ram import RAM
 from node.telemetry.metrics.disk import Disk
 from node.telemetry.metrics.session import Session
+from node.telemetry.metrics.battery import Battery
 
 
 def main():
@@ -23,10 +24,12 @@ def main():
     ram = RAM()
     disk = Disk()
     session = Session()
+    battery = Battery()
     heart.register_metric(cpu)
     heart.register_metric(ram)
     heart.register_metric(disk)
     heart.register_metric(session)
+    heart.register_metric(battery)
 
 
 if __name__ == '__main__':
---
+++ node/telemetry/metrics/battery.py
@@ -0,0 +1,33 @@
+"""
+Battery Metrics from psutil.
+"""
+from node.telemetry.metric import Metric
+import psutil
+import time
+
+
+class Battery(Metric):
+    """
+    Wrapper for psutil Battery information.
+    """
+
+    def metric_name(self) -> str:
+        return "battery"
+
+    def measure(self) -> dict:
+        try:
+            psubattery = psutil.sensors_battery()
+            if psubattery is None:
+                return {
+                        'percent': None,
+                        'secs_left': None,
+                        'power_plugged': None,
+                }
+            data = {
+                    'percent': psubattery.percent,
+                    'secs_left': psubattery.secsleft,
+                    'power_plugged': psubattery.power_plugged,
+            }
+            return data
+        except Exception as e:
+            raise ValueError(f'Unable to collect RAM metrics: {e}')
